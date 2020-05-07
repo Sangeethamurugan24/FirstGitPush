2814d47419156740dcecba4c4d8c0156b8cbbe38
Sangeetha <m.sangeetha244@gmail.com>
2020-05-07 13:33:41 +0530

    deleted a package

--- src/test/java/Test/test.java
+++ src/test/java/Test/test.java
@@ -4,12 +4,12 @@ import java.net.URL;
 
 import org.testng.annotations.Test;
 
-import Launch.HomePage;
+
 import Launch.LaunchApp;
 import io.appium.java_client.android.AndroidDriver;
 
-@Test
-public class test extends HomePage {
+
+public class test extends LaunchApp {
 	
 	public static void main(String[] args) {
 		LaunchApp ln=new LaunchApp() ;
